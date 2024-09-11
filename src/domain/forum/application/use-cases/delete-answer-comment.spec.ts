60b6ba19b9a89f6b0b1497532460b5726b47ffe7
biluadev <biluadevcod@gmail.com>
2024-09-11 18:30:12 +0100

    Added Functional Error Handling

--- src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryAnswerCommentsRepository } from '@/test/repositories/in-memory-
 import { DeleteAnswerCommentUseCase } from './delete-answer-comment'
 import { makeAnswerComment } from '@/test/factories/make-answer-comment'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
 let sut: DeleteAnswerCommentUseCase
@@ -34,14 +35,13 @@ describe('Delete Answer Comment', () => {
 
         await inMemoryAnswerCommentsRepository.create(answerComment)
 
-        expect(() => {
-            return sut.execute({
-                answerCommentId: answerComment.id.toString(),
-                authorId: 'author-2',
-            })
+        const result = await sut.execute({
+            answerCommentId: answerComment.id.toString(),
+            authorId: 'author-2',
         })
 
-        expect(inMemoryAnswerCommentsRepository.items).toHaveLength(0);
+        expect(result.isLeft()).toBe(true)
+        expect(result.value).toBeInstanceOf(NotAllowedError)
     });
 
 })
